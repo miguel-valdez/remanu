5d0a1d9a6a7c08ac56b24a50ef7bbca49e4a97c2
m. a. valdez-grijalva <miguel.valdesg@gmail.com>
2020-09-24 14:17:06 -0500

    added lab and sequences

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name        = 'remanu',
+      version     = '0.1',
+      description = 'fenics-based fem solver for nuclear magnetic resonance PDEs',
+      url         = 'http://github.com/miguel-valdez/remanu',
+      author      = 'Miguel A. Valdez G.',
+      author_email= 'mavaldez@imp.mx',
+      license     = 'LGPLv2.1',
+      packages    =['remanu'],
+      zip_safe    =  False,
+      )
