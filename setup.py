ebb4f7e598559e9d71d7fa0dcafa5f62e3b0896d
m. a. valdez-grijalva <miguel.valdesg@gmail.com>
2020-09-24 16:56:32 -0500

    modified setup

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-from setuptools import setup
+import setuptools
 
-setup(name        = 'remanu',
-      version     = '0.1',
-      description = 'fenics-based fem solver for nuclear magnetic resonance PDEs',
-      url         = 'http://github.com/miguel-valdez/remanu',
-      author      = 'Miguel A. Valdez G.',
-      author_email= 'mavaldez@imp.mx',
-      license     = 'LGPLv2.1',
-      packages    =['remanu'],
-      zip_safe    =  False,
-      )
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='remanu-miguelvg87',
+    version='0.1',
+    author='Miguel A. Valdez G.',
+    author_email='mavaldez@imp.mx',
+    description='fenics-based fem solver for nuclear magnetic resonance PDEs',
+    long_description=long_description,
+    long_description_content_type='text',
+    url='https://github.com/miguel-valdez/remanu',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: LGPL License',
+        'Operating System :: OS Independent',
+        ],
+    python_requires= '>=3.6',
+)
