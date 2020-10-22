6f85ffc3b4cc251dca9c01854a143f849e4363e4
m. a. valdez-grijalva <miguel.valdesg@gmail.com>
2020-10-21 22:52:36 -0500

    meshing

--- remanu/mesh.py
+++ remanu/mesh.py
@@ -2,7 +2,16 @@ import dolfin as fem
 import pygmsh as pg
 import meshio
 
-def gmsh_to_xdmf(mesh):
+def disk(x, y= 1., l= 0.1):
+    geom = pg.opencascade.Geometry(
+    )
+
+    geom.add_disk((0., 0., 0.), 1., radius1= y, char_length= l)
+
+    mesh = pg.generate_mesh(geom,
+        prune_z_0= True, verbose= 0, remove_lower_dim_cells= True,
+    )
+
     # Export to dolfin .xml
     meshio.write("/tmp/mesh.xdmf",
         meshio.Mesh(points= mesh.points,
@@ -14,20 +23,6 @@ def gmsh_to_xdmf(mesh):
     mesh = fem.Mesh()
     with fem.XDMFFile('/tmp/mesh.xdmf') as infile:
         infile.read(mesh)
-
-    return mesh
-
-def disk(x, y= 1., l= 0.1):
-    geom = pg.opencascade.Geometry(
-    )
-
-    geom.add_disk((0., 0., 0.), 1., radius1= y, char_length= l)
-
-    mesh = pg.generate_mesh(geom,
-        prune_z_0= True, verbose= 0, remove_lower_dim_cells= True,
-    )
-
-    mesh = gmsh_to_xdmf(mesh)
 
     mesh.scale(x)
 
